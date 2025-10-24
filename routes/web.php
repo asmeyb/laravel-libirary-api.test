c53fdbfe95c5a307e0d5fefd822e67ec24e9a6fc
asmeyb <asmeyb@gmail.com>
2025-10-24 16:22:46 +0300

    Samctum Installation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('Authors/index', AuthorController::class)->name('Authors.index');
